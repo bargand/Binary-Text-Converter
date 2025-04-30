95f79bb867c7e8a2d814cd5bc1c150f656905292
Debargha Nandi <debarghanandi.india@gmail.com>
2025-04-30 21:59:18 +0530

    Update

---
+++ script.js
@@ -0,0 +1,29 @@
+function convert() {
+  let input = document.getElementById("textInput").value;
+  let conversionType = document.getElementById("conversionType").value;
+  let output = "";
+
+  if (conversionType === "textToBinary") {
+    output = input
+      .split("")
+      .map((char) => char.charCodeAt(0).toString(2).padStart(8, "0"))
+      .join(" ");
+  } else {
+    output = input
+      .split(" ")
+      .map((bin) => String.fromCharCode(parseInt(bin, 2)))
+      .join("");
+  }
+
+  document.getElementById("output").value = output;
+}
+function clearInput() {
+  document.getElementById("textInput").value = "";
+  document.getElementById("output").value = "";
+}
+
+function copyOutput() {
+  let outputText = document.getElementById("output");
+  outputText.select();
+  document.execCommand("copy");
+}
